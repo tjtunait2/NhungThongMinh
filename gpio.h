164692418df8e6a36702405deb948987dd222607
tjtunait2 <youwhyimalive@gmail.com>
2022-06-04 08:26:42 +0700

    fix bug

--- gpio.h
+++ gpio.h
@@ -2,10 +2,7 @@
 #define GPIO_H
 int gpio_export(unsigned gpio); //Hàm export pin ra user space
 int gpio_unexport(unsigned gpio); //Hàm giải phóng pin khi không còn
-sử dụng
 int gpio_dir_out(unsigned gpio); //Cấu hình pin là output
 int gpio_dir_in(unsigned gpio); //Cấu hình pin là inputThí nghiệm IT4425 – Phát triển phần mềm nhúng thông minh
-SOICT-HUST 11
 int gpio_value(unsigned gpio, unsigned value); //Đọc/ghi giá trị của
-pin gpio
 #endif
